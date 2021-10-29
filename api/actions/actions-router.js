5124cdcd4c1a66e5f20908e8a51d4c466fb5cc33
Ryan Mark <rymark907@gmail.com>
2021-10-29 12:32:11 -0700

    can update actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -40,6 +40,15 @@ router.post('/', checkActionsBody, async (req, res, next) => {
     }
 });
 
+router.put('/:id', checkActionsBody, checkActionsId, async (req, res, next) => {
+    try {
+        const updatedAction = await Actions.update(req.params.id, req.body);
+        res.status(200).json(updatedAction);
+    } catch (err) {
+        next(err);
+    }
+})
+
 router.use(errorHandler);
 
 module.exports = router;
