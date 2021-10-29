2f94e778e1551fd730d53da9964daa2c0dbfc5b4
Ryan Mark <rymark907@gmail.com>
2021-10-29 11:00:37 -0700

    Able to update existing projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -35,6 +35,15 @@ router.post('/', checkProjectsBody, async (req, res, next) => {
     } catch (err) {
         next(err);
     }
+});
+
+router.put('/:id', checkProjectsBody, checkProjectsId, async (req, res, next) => {
+    try {
+        const updatedProject = await Projects.update(req.params.id, req.body);
+        res.status(200).json(updatedProject);
+    } catch (err) {
+        next(err);
+    }
 })
 
 router.use(errorHandler);
